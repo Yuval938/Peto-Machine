9749e59116ea18123cd9be5172187bebbf6fb8be
Yuval938 <59195998+Yuval938@users.noreply.github.com>
2021-09-19 15:28:00 +0300

    adding schedule and updating schedule every 30 sec

--- Scheduler.py
+++ Scheduler.py
@@ -74,7 +74,7 @@ class Scheduler(IScheduler):
         pass
 
     def normalRoutine(self):
-        schedule.every(30).seconds.do(check_for_new_schedule).tag("normalRoutine")
+        schedule.every(30).seconds.do(check_for_new_schedule,self.peto).tag("normalRoutine")
         schedule.every(4).seconds.do(should_I_Feed, self.peto).tag("normalRoutine")
         # schedule.every(4).minutes.do(feed, self.peto,30)
         while 1:
